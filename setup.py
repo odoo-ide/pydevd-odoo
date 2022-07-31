923d8f71123abac5786ba0fc1588f54d00dc51f2
Trinh Anh Ngoc <atw1990@gmail.com>
2022-07-31 21:40:05 +0700

    1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydevd-odoo',
-    version='1.1',
+    version='1.2',
     description='PyDev.Debugger plugin for Odoo',
     url='https://github.com/odoo-ide/pydevd-odoo',
     author='Trinh Anh Ngoc',
