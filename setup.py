8049654cc293e16e139ca9122b9cbf8c2bebf395
Trinh Anh Ngoc <atw1990@gmail.com>
2022-08-01 14:11:31 +0700

    Switch to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='pydevd-odoo',
-    version='1.2',
-    description='PyDev.Debugger plugin for Odoo',
-    url='https://github.com/odoo-ide/pydevd-odoo',
-    author='Trinh Anh Ngoc',
-    author_email='atw1990@gmail.com',
-    packages=find_packages(),
-    namespace_packages=['pydevd_plugins.extensions'],
-    license='MIT',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown'
-)
+setup()
